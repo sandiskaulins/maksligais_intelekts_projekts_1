71d9ff0d3fe667d76c20ec614e2431cc222d4f7d
Sandis Kauliņš <144373070+sandiskaulins@users.noreply.github.com>
2025-03-25 19:59:09 +0200

    Update solution.py

--- solution.py
+++ solution.py
@@ -137,19 +137,19 @@ def heiristiska_novertejuma_funkcija(starting_player, num_string_len, points, ba
     # Katram jāimplementē viens faktors
     print("")
     
-""""" def heiristiska_novertejuma_funkcija(starting_player, num_string_len, points, bank):
+def heiristiska_novertejuma_funkcija(starting_player, num_string_len, points, bank):
     score = 0
-        if starting_player == "dators":
-            if points % 2 == 0 and bank % 2 == 0: # ja dators sāk spēli, tad tā mērķis ir iegūt gan kop. skaitu, gan bankas kā pāra skaitli
-                score += 10 # += 10 nozīmē, ka šis stāvoklis ir izdevīgs datoram, jo tas mēģina lai abi būtu pāra skaitļi, tādējadi +10 ir heiristiskas funkcijas vērtējums 
-            elif points % 2 == 1 and bank % 2 == 1: # ja abi skaitļi ir nepāra, tad šis nav izdevīgi datoram, jo tad uzvarēs cilvēks
-                score -=10 # līdz ar to heiristiskas funkcijas vērtējums ir -10
+    if starting_player == "dators":
+        if points % 2 == 0 and bank % 2 == 0: # ja dators sāk spēli, tad tā mērķis ir iegūt gan kop. skaitu, gan bankas kā pāra skaitli
+            score += 10 # += 10 nozīmē, ka šis stāvoklis ir izdevīgs datoram, jo tas mēģina lai abi būtu pāra skaitļi, tādējadi +10 ir heiristiskas funkcijas vērtējums 
+        elif points % 2 == 1 and bank % 2 == 1: # ja abi skaitļi ir nepāra, tad šis nav izdevīgi datoram, jo tad uzvarēs cilvēks
+            score -=10 # līdz ar to heiristiskas funkcijas vērtējums ir -10
                 
-            else: # ja cilvēks sāk spēli
-                if points % 2 == 1 and bank % 2 == 1:
-                    score +=10 # šis stavoklis ir labvēlīgs datoram, gadījumā ja cilvēks sāk spēli, jo datora mērķi ir iegūt nepār skaitļus
-                 elif points % 2 == 0 and bank % 2 == 0:
-                     score -= 10 # šis stāvoklis ir nelabvēlīgs datoram, līdz ar to heiristiskas funkc vērtējums -10
+        else: # ja cilvēks sāk spēli
+            if points % 2 == 1 and bank % 2 == 1:
+                score +=10 # šis stavoklis ir labvēlīgs datoram, gadījumā ja cilvēks sāk spēli, jo datora mērķi ir iegūt nepār skaitļus
+            elif points % 2 == 0 and bank % 2 == 0:
+                score -= 10 # šis stāvoklis ir nelabvēlīgs datoram, līdz ar to heiristiskas funkc vērtējums -10
 
     return score
     # šis faktors ir domāts tā, ka dators cenšas izvairīties no tiem gājieniem, kuri dos -1 punktu, jo tas neļaus datoram kontrolēt spēles gaitu, vēlāk būs grūtāk atgriezties pie pāra sk.
@@ -170,7 +170,7 @@ def main():
     print("Sākotnējā virkne:", root.num_string)
 
     for x in tree.nodes_list:
-        print(x.id,x.num_string,x.points,x.bank,x.level,x.player)
+        print(x.id,x.num_string,x.points,x.bank,x.level)
     
     for x, y in tree.arc_dict.items():
         print(x, y)   
